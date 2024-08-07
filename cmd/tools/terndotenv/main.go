726c142a2d25f0427040bc42a927a462b69573f0
fquinteiro <fquinteiro@gmail.com>
2024-08-07 17:43:48 +0100

    Docker compose, postgres and migrations setup

---
+++ cmd/tools/terndotenv/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+// func main() {
+// 	if err := godotenv.Load(); err != nil {
+// 		panic(err)
+// 	}
+
+// 	cmd := exec.Command("cmd", "/c", "tern", "migrate", "--migrations", "./internal/store/pgstore/migrations", "--config", "./internal/store/pgstore/migrations/tern.conf")
+
+// 	if err := cmd.Run(); err != nil {
+// 		panic(err)
+// 	}
+// }
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(
+		"tern",
+		"migrate",
+		"--migrations",
+		"./internal/store/pgstore/migrations",
+		"--config",
+		"./internal/store/pgstore/migrations/tern.conf",
+	)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Command execution failed with error: %v\n", err)
+		fmt.Printf("Output: %s\n", string(output))
+		panic(err)
+	}
+
+	fmt.Printf("Command executed successfully: %s\n", string(output))
+}
